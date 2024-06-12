d4a7745797f27f37628e1c33f94fa7f3854a8aaf
Guy Twa <gtwa@uab.edu.com>
2024-06-12 11:44:20 -0400

    styling

--- R/get_PMID.R
+++ R/get_PMID.R
@@ -7,8 +7,8 @@
 #' get_PMID
 get_PMID <- function() {
     if (!require(rentrez)) {
-  install.packages("rentrez")
-}
+        install.packages("rentrez")
+    }
     # Search for the last 100 articles published related to genetics
     search_results <- rentrez::entrez_search(db = "pubmed", term = "genetics", retmax = 100)
 
