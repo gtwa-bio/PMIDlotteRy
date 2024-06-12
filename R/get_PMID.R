3e3234aed83296ee33323342dcb1052a8371f27b
Guy Twa <gtwa@uab.edu.com>
2024-06-12 08:59:06 -0400

    style updates

--- R/get_PMID.R
+++ R/get_PMID.R
@@ -6,12 +6,12 @@
 #' @examples
 #' get_PMID
 get_PMID <- function() {
-  # Search for the last 100 articles published related to genetics
-  search_results <- rentrez::entrez_search(db="pubmed", term="genetics", retmax=100)
+    # Search for the last 100 articles published related to genetics
+    search_results <- rentrez::entrez_search(db = "pubmed", term = "genetics", retmax = 100)
 
-  # Select a random PMID from the search results
-  set.seed(as.integer(gsub("-", "", Sys.Date())))
-  PMID <- sample(search_results$ids, 1)
+    # Select a random PMID from the search results
+    set.seed(as.integer(gsub("-", "", Sys.Date())))
+    PMID <- sample(search_results$ids, 1)
 
-  return(as.numeric(PMID))
+    return(as.numeric(PMID))
 }
