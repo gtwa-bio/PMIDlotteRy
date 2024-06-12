ef1ccc257fb8146c670b3f77c35ae7f4eaf47e50
Guy Twa <gtwa@uab.edu.com>
2024-06-12 11:31:26 -0400

    I put the require statement back, because everyone complained about it not being there.

--- R/get_PMID.R
+++ R/get_PMID.R
@@ -6,6 +6,9 @@
 #' @examples
 #' get_PMID
 get_PMID <- function() {
+    if (!require(rentrez)) {
+  install.packages("rentrez")
+}
     # Search for the last 100 articles published related to genetics
     search_results <- rentrez::entrez_search(db = "pubmed", term = "genetics", retmax = 100)
 
