25228ac3298dff3f909b1c125ef53075fe44623f
Guy Twa <gtwa@uab.edu.com>
2024-06-13 15:20:12 -0400

    Vignette building is failing. I think its because my dependency for rentrez is not being implemented well. Updating get_PMID

--- R/get_PMID.R
+++ R/get_PMID.R
@@ -6,9 +6,7 @@
 #' @examples
 #' get_PMID
 get_PMID <- function() {
-    if (!require(rentrez)) {
-        install.packages("rentrez")
-    }
+    library(rentrez)
     # Search for the last 100 articles published related to genetics
     search_results <- rentrez::entrez_search(db = "pubmed", term = "genetics", retmax = 100)
 
