23fd0126bb8362ccebfe6f96467c6392a8be1014
Guy Twa <gtwa@uab.edu.com>
2024-06-13 19:38:01 -0400

    fixing depends, and adding pkkgdown to buildignore

--- R/get_PMID.R
+++ R/get_PMID.R
@@ -2,10 +2,10 @@
 #'
 #' @return PMID for today
 #' @importFrom rentrez entrez_search
-#'
+#' @export
 #'
 #' @examples
-#' get_PMID
+#' get_PMID()
 get_PMID <- function() {
     # Search for the last 100 articles published related to genetics
     search_results <- rentrez::entrez_search(db = "pubmed", term = "genetics", retmax = 100)
