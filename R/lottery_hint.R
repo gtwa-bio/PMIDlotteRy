4a66aa0db633773b9f6e43d8b9b1d9aa2fb6a865
Guy Twa <gtwa@uab.edu.com>
2024-06-14 10:59:31 -0400

    added subject parameter to documentation, function call, and return statement.

--- R/lottery_hint.R
+++ R/lottery_hint.R
@@ -1,16 +1,18 @@
 #' Hint for today's numbers
+#' 
+#' @param subject a string for lotto category
 #'
 #' @return A hint for today's possible PMID range
 #' @export
 #'
 #' @examples
 #' lottery_hint()
-lottery_hint <- function(){
+lottery_hint <- function(subject = "genomics"){
   # Search for the last 100 articles published related to genetics
-  search_results <- rentrez::entrez_search(db = "pubmed", term = "genetics", retmax = 100)
+  search_results <- rentrez::entrez_search(db = "pubmed", term = subject, retmax = 100)
   PMID_floor <- floor(as.numeric(min(search_results$ids)) / 10) * 10
   PMID_ceiling <- ceiling(as.numeric(max(search_results$ids)) / 10) * 10
 
 
-  print(paste0("Today's PMIDs range: ", PMID_floor, "-", PMID_ceiling))
+  print(paste0("Today's PMID range in ", subject ,": ", PMID_floor, "-", PMID_ceiling))
 }
