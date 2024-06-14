5a93f94b15d290d2ffc3d5ee723b5769b25b14f3
Guy Twa <gtwa@uab.edu.com>
2024-06-14 13:00:28 -0400

    Added date range to search, to restrict search to papers from yesterday to 20 years ago. This resolves an issue where get_PMID would return different results throughout the day, as the database was updated.

--- R/get_PMID.R
+++ R/get_PMID.R
@@ -4,13 +4,32 @@
 #'
 #' @return PMID for today
 #' @import rentrez
+#' @import lubridate
 #' @export
 #'
 #' @examples
 #' get_PMID()
+#' get_PMID("neurobiology")
 get_PMID <- function(subject = "genomics") {
-    # Search for the last 100 articles published related to genetics
-    search_results <- rentrez::entrez_search(db = "pubmed", term = subject, retmax = 100)
+    # Calculate yesterday's date
+    yesterday <- Sys.Date() - 1
+
+    # Calculate the date 20 years ago from yesterday
+    twenty_years_ago <- format(yesterday - years(20), "%Y/%m/%d")
+
+    # Format yesterday's date
+    yesterday_formatted <- format(yesterday, "%Y/%m/%d")
+
+    # Construct the search term with the date range
+    subject_with_date_range <- sprintf("%s AND (%s:%s[Date - Publication])",
+                                    subject,
+                                    twenty_years_ago,
+                                    yesterday_formatted)
+
+    # Perform the search with the restricted date range
+    search_results <- rentrez::entrez_search(db = "pubmed",
+                                            term = subject_with_date_range,
+                                            retmax = 500)
 
     # Select a random PMID from the search results
     set.seed(as.integer(gsub("-", "", Sys.Date())))
