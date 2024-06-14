4fe12c199ccb3e2c71e7bd386fc222854a6028eb
Guy Twa <gtwa@uab.edu.com>
2024-06-14 10:35:43 -0400

    Updated to return a payout amount and information about the day's paper

--- R/lottery.R
+++ R/lottery.R
@@ -2,21 +2,42 @@
 #'
 #' @param lotto_numbers a integer vector of PMID guesses
 #'
-#' @return Any guess matches today's PMID
+#' @importFrom rentrez entrez_fetch
+#' @importFrom XML xpathSApply
+#' @importFrom XML xmlParse
+#' 
+#' @return Whether or not numbers match today's PMID, and information on the paper
 #' @export
 #'
 #' @examples
 #' lottery(38863995)
 #' lottery(c(38863995, 38864247, 38862712, 38862494))
 #'
-lottery <- function(lotto_numbers) {
+lottery <- function(lotto_numbers, subject = "genomics") {
     # Select a PMID based on the date
     PMID_today <- get_PMID()
 
+    # Fetch the PubMed record for the winning PMID
+    record_xml <- entrez_fetch(db = "pubmed", id = PMID_today, rettype = "xml")
+    record <- XML::xmlParse(record_xml)
+
+    # Extract title and abstract
+    title <- XML::xpathSApply(record, "//ArticleTitle", xmlValue)
+    abstract <- XML::xpathSApply(record, "//AbstractText", xmlValue)
+
+    # Set payout
+    set.seed(as.integer(gsub("-", "", Sys.Date())))
+    payout <- sample(1e+5:1e+10, 1) / length(lotto_numbers)
+
     # Check if the generated PMID is in the input vector
     if (PMID_today %in% lotto_numbers) {
-        return("Congratulations! You have won the lottery!")
+        print("Congratulations! You have won the lottery!")
+        print(paste0( "You've won: $", payout))
     } else {
-        return("Sorry, better luck next time.")
+        print("Sorry, better luck next time.")
     }
+
+    print((paste0("Check out today's paper in ", subject, "!")))
+    print(paste0("Title: ", title))
+    print(paste0("Abstract: ", abstract))
 }
