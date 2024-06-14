ddcb49a18e641589544e501f4ccce2d08a5379d2
Guy Twa <gtwa@uab.edu.com>
2024-06-14 10:38:00 -0400

    now accepts subject as an argument for PMID search

--- R/get_PMID.R
+++ R/get_PMID.R
@@ -6,9 +6,9 @@
 #'
 #' @examples
 #' get_PMID()
-get_PMID <- function() {
+get_PMID <- function(subject = "genomics") {
     # Search for the last 100 articles published related to genetics
-    search_results <- rentrez::entrez_search(db = "pubmed", term = "genetics", retmax = 100)
+    search_results <- rentrez::entrez_search(db = "pubmed", term = subject, retmax = 100)
 
     # Select a random PMID from the search results
     set.seed(as.integer(gsub("-", "", Sys.Date())))
